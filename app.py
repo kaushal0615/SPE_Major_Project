dfe47363af219ec2de882c742fdf1d1521b05875
Kaushigithub <kypancholi2@gmail.com>
2024-07-20 10:31:04 +0530

    first

---
+++ app.py
@@ -0,0 +1,57 @@
+import logging
+from logging.handlers import RotatingFileHandler
+from flask import Flask, request, jsonify
+from model_loader import load_model, load_scaler
+from elasticSearch_log import push_logs_to_elasticsearch
+import datetime
+
+app = Flask(__name__)
+
+# Load the trained model and scaler
+model = load_model('best_model.pickle')
+scaler = load_scaler('scaler.pickle')
+
+# Configure logging to use the existing log file 'logging.log'
+log_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+log_handler = RotatingFileHandler('logging.log', maxBytes=10240, backupCount=10)
+log_handler.setFormatter(log_formatter)
+app.logger.addHandler(log_handler)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Extract values from the request JSON
+        request_data = request.json
+        app.logger.info("Welcome to breast cancer prediction!")
+        
+        # Extract feature values from the request data
+        feature_names = ['worst perimeter', 'worst concave points', 'mean concave points', 'mean concavity', 'worst radius']
+        data = [request_data.get(feature) for feature in feature_names]
+
+        # Ensure the correct number of features
+        if len(data) != 5:
+            return jsonify({"error": "Expected 5 input values"}), 500
+            # app.logger.info("Prediction cannot be processed due to inappropriate input")
+
+        # Transform input data using the same scaler used during training
+        final_input = scaler.transform([data])
+
+        # Make prediction
+        output = int(model.predict(final_input)[0])
+
+        # Log prediction result to both file and Elasticsearch
+        app.logger.info("Prediction Successfull!")
+        prediction_text = "The Cancer is Benign" if output == 0 else "The Cancer is Malignant"
+        app.logger.info(prediction_text)
+        # es.index(index="predictions", body={"prediction_text": prediction_text})
+        push_logs_to_elasticsearch()
+
+        # Return prediction result as JSON
+        return jsonify({"prediction_text": prediction_text}), 200
+    
+    except Exception as e:
+        app.logger.info(f"Error occurred during prediction due to insufficient data")
+        return jsonify({"error": "Internal Server Error"}), 400
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=2060, debug=True)
